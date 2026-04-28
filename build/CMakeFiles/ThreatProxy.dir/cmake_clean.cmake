e7126914de19f440f5f9d5ffa3ad8c3f438ee174
NR-NJN <niranjan.bengaluru@gmail.com>
2026-04-28 13:49:25 -0400

    wifi module verification

---
+++ build/CMakeFiles/ThreatProxy.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "ThreatProxy.map"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/main.c.o"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/main.c.o.d"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/stm32l4xx_hal_msp.c.o"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/stm32l4xx_hal_msp.c.o.d"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/stm32l4xx_it.c.o"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/stm32l4xx_it.c.o.d"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/syscalls.c.o"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/syscalls.c.o.d"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/sysmem.c.o"
+  "CMakeFiles/ThreatProxy.dir/Core/Src/sysmem.c.o.d"
+  "CMakeFiles/ThreatProxy.dir/startup_stm32l475xx.s.o"
+  "ThreatProxy"
+  "ThreatProxy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/ThreatProxy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
